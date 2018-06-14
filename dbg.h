bbbb65e3b985c9eced33eb6f37dbb45d1b9d90d2
Lee Raulin <leeraulin@gmail.com>
2018-06-14 13:13:05 -0400

    add ex23

--- dbg.h
+++ dbg.h
@@ -1,5 +1,6 @@
 #ifndef __dbg_h__
 #define __dbg_h__
+
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
@@ -7,17 +8,22 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d " M "\n", \
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", \
                               __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) fprintf(stderr,                                                   \
+                                "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, \
+                                clean_errno(), ##__VA_ARGS__)
 
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) fprintf(stderr,                              \
+                                 "[WARN] (%s:%d: errno: %s) " M "\n", \
+                                 __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n ", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", \
+                                 __FILE__, __LINE__, ##__VA_ARGS__)
 
 #define check(A, M, ...)       \
   if (!(A))                    \
